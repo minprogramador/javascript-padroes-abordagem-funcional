5ac407b3240304092b48a5e73915f4cdbb92c644
work <work@Mac-mini.local>
2019-08-10 13:58:37 -0300

    limitar operacaoes, duplo click -- debounceTime

--- public/app/app.js
+++ public/app/app.js
@@ -2,13 +2,15 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as Service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation = takeUntil(3, () => {
-    return Service
+const acc = takeUntil(3, () => 
+    Service
     .totalItens('2143')
     .then(console.log)
     .catch(console.log)
-});
+);
 
-document.querySelector('#myButton').onclick = operation;
+const action = debounceTime(500, acc);
+
+document.querySelector('#myButton').onclick = action;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -15,3 +15,11 @@ export const takeUntil = (times, fn) => {
         if(times-- > 0) fn();
     }
 }
+
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, milliseconds);
+    }
+}
