2069d223070587d4a350940c6e6d3d9ca80b99e4
work <work@Mac-mini.local>
2019-08-10 11:53:58 -0300

    implementando a funcao log

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,5 @@
 
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
 
 
 document
@@ -8,10 +8,7 @@ document
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
     .then(notas => notas.reduce( (array, nota) => array.concat(nota.itens), [] ))
-    .then(itens => {
-        console.log(itens);
-        return itens;
-    })
+    .then(log)
     .then(itens => itens.filter(item => item.codigo == '2143'))
     .then(itens => itens.reduce( (total, item) => total + item.valor, 0 ))
     .then(notas => console.log(notas))
