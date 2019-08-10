39bb52d2a57c5ced3bb068ea5896e928ebcb0544
work <work@Mac-mini.local>
2019-08-10 12:09:15 -0300

    melhorando codigo apos flatMap

--- public/app/app.js
+++ public/app/app.js
@@ -8,9 +8,10 @@ document
 .onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => notas.$flatMap(nota => nota.itens))
-    .then(itens => itens.filter(item => item.codigo == '2143'))
-    .then(log)
-    .then(itens => itens.reduce( (total, item) => total + item.valor, 0 ))
+    .then(notas => notas
+        .$flatMap(nota => nota.itens)
+        .filter(item => item.codigo == '2143')
+        .reduce( (total, item) => total + item.valor, 0 )
+    )
     .then(console.log)
     .catch(console.log)
